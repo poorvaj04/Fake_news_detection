a65ea1bb4ce499139fba262c706bb1c2e3dae28d
poorvaj04 <poorvajraja7@gmail.com>
2025-06-07 21:18:04 +0530

    app.py

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ def predict_news(text):
         outputs = model(**inputs)
         probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
     label = torch.argmax(probs).item()
-    return "❌ Fake News" if label == 1 else "✅ Real News"
+    return "✅ Real News" if label == 1 else "❌ Fake News"
 
 # Input
 with st.container():
